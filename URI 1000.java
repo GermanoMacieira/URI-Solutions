01aff06823155aa36c40ffbcbbd39fd225e19384
Germano Macieira <69836135+GermanoMacieira@users.noreply.github.com>
2021-02-24 19:02:00 -0300

    Add files via upload

---
+++ URI 1000.java
@@ -0,0 +1,26 @@
+/*
+
+QUESTÃO URI 1000:
+
+O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". 
+Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+
+Entrada:
+Este problema não possui nenhuma entrada.
+
+Saída:
+Você deve imprimir a mensagem "Hello World!" conforme o exemplo abaixo. 
+
+*/
+
+import java.io.IOException;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+         System.out.println("Hello World!");
+ 
+    }
+ 
+}
