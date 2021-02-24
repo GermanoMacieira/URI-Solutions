5bb51d8ea0ea4bdaf190d39471f33f1553ea42d5
Germano Macieira <69836135+GermanoMacieira@users.noreply.github.com>
2021-02-24 16:33:59 -0300

    Create URI 1000.java

---
+++ URI 1000.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        // URI 1000 Hello World!
+
+         System.out.println("Hello World!");
+ 
+    }
+ 
+}
